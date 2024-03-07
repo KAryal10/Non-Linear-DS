37345db14598659ecda8da12e8822fea9820b510
KAryal10 <147074025+KAryal10@users.noreply.github.com>
2024-03-07 11:34:13 -0500

    uploaded assignment 2

---
+++ hw 2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "CharacterFrequencies.h"
+using namespace std;
+
+
+
+int main(){
+    string line2 = "Twinkle twinkle little star, how I wonder what you are.";
+    cout << line2 << endl;
+    cout<<"asd"<<endl;
+    CharacterFrequencies line2CharFreq(line2);
+    line2CharFreq.printFrequencies();
+    
+    line2CharFreq.readString(line2);
+    line2CharFreq.printFrequencies();
+    
+    return 0;
+}
